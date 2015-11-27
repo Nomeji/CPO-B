fb48928587f23abe74a064bd9745ed0d0394b327
Nomeji <nomeji244@gmail.com>
2015-11-27 10:59:45 +0100

    tests et ex2

--- TP3/src/edu/iut/app/ExamEvent.java
+++ TP3/src/edu/iut/app/ExamEvent.java
@@ -9,7 +9,11 @@ public class ExamEvent {
 	
 	public ExamEvent(Date date, Person person, ArrayList<Person> jury,
 					Classroom classRoom, ArrayList<Document> document) {
-		
+	examDate= date;
+	student=person;
+	this.jury=jury;
+	documents=document;
+	classroom=classRoom;
 	}
 	
 	/** EX2: FAITE LES ACCESSEURS DES ATTRIBUTS, AJOUTER DES ATTRIBUT ? **/
@@ -18,5 +22,46 @@ public class ExamEvent {
 	protected ArrayList<Person> jury;
 	protected Classroom classroom;
 	protected ArrayList<Document> documents;
+	
+	//Getteur et Setteur
+	public Date getExamDate() {
+		return examDate;
+	}
+
+	public void setExamDate(Date examDate) {
+		this.examDate = examDate;
+	}
+
+	public Person getStudent() {
+		return student;
+	}
+
+	public void setStudent(Person student) {
+		this.student = student;
+	}
+
+	public ArrayList<Person> getJury() {
+		return jury;
+	}
+
+	public void setJury(ArrayList<Person> jury) {
+		this.jury = jury;
+	}
+
+	public Classroom getClassroom() {
+		return classroom;
+	}
+
+	public void setClassroom(Classroom classroom) {
+		this.classroom = classroom;
+	}
+
+	public ArrayList<Document> getDocuments() {
+		return documents;
+	}
+
+	public void setDocuments(ArrayList<Document> documents) {
+		this.documents = documents;
+	}
 	 
 }
---
+++ TP3/src/edu/iut/tools/IUTSchedulerTest.java
@@ -0,0 +1,14 @@
+package edu.iut.tools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IUTSchedulerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
